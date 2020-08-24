c04453d8a3e4928a5cf9aae90ffa41ecedfe144d
Peyman Ghazvini <paymanghazvini@yahoo.com>
2020-08-24 16:31:16 +0400

    fix(dist): dist added to repository

---
+++ dist/utils/get-watermark-position.js
@@ -0,0 +1,53 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getWatermarkPosition = void 0;
+
+var getWatermarkPosition = function getWatermarkPosition() {
+  var position = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'center';
+  var canvas = arguments.length > 1 ? arguments[1] : undefined;
+  var width = arguments.length > 2 ? arguments[2] : undefined;
+  var height = arguments.length > 3 ? arguments[3] : undefined;
+  var canvasRect = canvas.getBoundingClientRect();
+  var centerPositionX = canvasRect.width / 2 - width / 2;
+  var centerPositionY = canvasRect.height / 2 - height / 2;
+
+  if (position === 'center') {
+    return [centerPositionX, centerPositionY];
+  }
+
+  position = position.split('-');
+  var rightPosition = canvasRect.width - width;
+  var bottomPosition = canvasRect.height - height;
+  return position.map(function (p, i) {
+    if (p === 'center') {
+      return i === 0 ? centerPositionX : centerPositionY;
+    }
+
+    if (p === 'right') {
+      return rightPosition;
+    }
+
+    if (p === 'bottom') {
+      return bottomPosition;
+    } // If top or left or unknown value would return 0 as 0 the right position for left & top.
+
+
+    return 0;
+  });
+};
+
+exports.getWatermarkPosition = getWatermarkPosition;
+;
+
+var _temp = function () {
+  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+    return;
+  }
+
+  __REACT_HOT_LOADER__.register(getWatermarkPosition, "getWatermarkPosition", "/Users/peymanghazvini/Desktop/filerobot-image-editor/projects/react/utils/get-watermark-position.js");
+}();
+
+;
---
+++ dist/utils/map-number-range.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _default = Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
+var _default2 = _default;
+exports.default = _default2;
+;
+
+var _temp = function () {
+  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+    return;
+  }
+
+  __REACT_HOT_LOADER__.register(_default, "default", "/Users/peymanghazvini/Desktop/filerobot-image-editor/projects/react/utils/map-number-range.js");
+}();
+
+;
